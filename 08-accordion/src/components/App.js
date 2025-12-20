0fc168c246df27342f1f12e5eb4b53b6ce8f896e
marykuo <meiyu19980928@gmail.com>
2025-12-20 11:46:08 +0800

    initial react app

---
+++ 08-accordion/src/components/App.js
@@ -0,0 +1,11 @@
+import Accordion from "./Accordion.js";
+import faqs from "./Data.js";
+
+export default function App() {
+  console.log(faqs);
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+}
