ffc3eea488fdf047f632463266d709239c54a263
marykuo <meiyu19980928@gmail.com>
2025-12-20 13:37:38 +0800

    feat: only have one item opened at a time

--- 08-accordion/src/components/Accordion.js
+++ 08-accordion/src/components/Accordion.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import AccordionItem from "./AccordionItem";
 
 export default function Accordion({ data }) {
+  const [currentOpen, setCurrentOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((faq, i) => (
@@ -8,6 +11,8 @@ export default function Accordion({ data }) {
           number={i + 1}
           title={faq.title}
           text={faq.text}
+          currentOpen={currentOpen}
+          setCurrentOpen={setCurrentOpen}
           key={faq.title}
         />
       ))}
--- 08-accordion/src/components/AccordionItem.js
+++ 08-accordion/src/components/AccordionItem.js
@@ -1,14 +1,21 @@
-import { useState } from "react";
+export default function AccordionItem({
+  number,
+  title,
+  text,
+  currentOpen,
+  setCurrentOpen,
+}) {
+  const isOpen = number === currentOpen;
 
-export default function AccordionItem({ number, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+  function handleToggle(number) {
+    setCurrentOpen(isOpen ? null : number);
   }
 
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => handleToggle(number)}
+    >
       <p className="number">{number < 9 ? `0${number}` : number}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
