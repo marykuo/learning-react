ccb16da679ce04e40871dee2e571b94e2a55ad93
marykuo <meiyu19980928@gmail.com>
2025-12-20 10:36:31 +0800

    feat: update stats by using derived state

--- 06-travel-list/src/App.js
+++ 06-travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -112,10 +112,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+  }
+
+  const totalItem = items.length;
+  const packedItem = items.filter((item) => item.packed).length;
+  const packedPercentage = Math.round((packedItem / totalItem) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already package X (X%)</em>
+      <em>
+        {packedPercentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `You have ${totalItem} items on your list, and you already package ${packedItem} (${packedPercentage}%)`}
+      </em>
     </footer>
   );
 }
