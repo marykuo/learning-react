2714a95a200baf56ecbd00cff109d02bf81b3968
marykuo <meiyu19980928@gmail.com>
2025-12-20 12:50:15 +0800

    feat: handle toggle event

--- 08-accordion/src/components/AccordionItem.js
+++ 08-accordion/src/components/AccordionItem.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 export default function AccordionItem({ number, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
-    <div className="item">
-      <p className="number">{number}</p>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{number < 9 ? `0${number}` : number}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
